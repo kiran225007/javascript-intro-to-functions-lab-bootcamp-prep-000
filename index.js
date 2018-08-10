ad506ac914578ae1c76d0e36c620ad3ca73a8a03
kiran225007@gmail.com <kiran225007@gmail.com>
2018-08-10 03:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function logshout(string){
+function logShout(string){
   var str = "hello"
   console.log(str.toUpperCase())
 return string.toUpperCase()
+}
+
+function logWhisper(string)
+{
+  var str = "Hello"
+  console.log(str.toLowerCase())
+  
 }
